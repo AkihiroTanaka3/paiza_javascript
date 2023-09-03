efda7efe7beb41ed57ea074e3c94d607f449385c
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-09-03 22:24:04 +0900

    サイコロ

---
+++ test1.js
@@ -0,0 +1,7 @@
+// 数の表示とサイコロ
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var randnum = parseInt(Math.random() * 10) +1;
+// console.log(randnum);
+// var number = 100;
+console.log("スライムが" + randnum + "匹あらわれた");
